443469240eb05d030a4a4f9abcf795da6933a252
pradeepodela <72432569+pradeepodela@users.noreply.github.com>
2022-07-22 16:19:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+import openai
+
+openai.api_key = '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'
+
+
+st.title('Grammar correction app')
+st.subheader('EX:- She no went to the market.')
+txt = st.text_area('Enter text to correct')
+if st.button('Correct'):
+    response = openai.Completion.create(
+  model="text-davinci-002",
+  prompt=f"Correct this to standard English:\n\n{txt}",
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+    
+    st.write(response.choices[0].text)
+    print(response.choices[0].text)
